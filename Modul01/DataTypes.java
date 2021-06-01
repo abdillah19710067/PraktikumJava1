14e0ec07a69928f303aa6217b049e0a8cabd17a5
abdillah19710067 <abdillahabdillah112@gmail.com>
2021-06-01 17:33:18 +0800

    deklarasi variabel dan tipe data

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,11 @@
+public class DataTypes{
+ public static void main(String[]args){
+  String namaLengkap = "Abdillah";
+  String namaPanggilan = "Dillah";
+  int usia = 18;
+  int targetTahunKuliah = 4;
+  double ipk = 3.89764512;
+  char nilaiAbjad = 'A' ;
+  boolean tampan = true;
+ }
+}
