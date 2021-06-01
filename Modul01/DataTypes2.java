afd1ff069956993c1ad501b2a64281e95c262767
abdillah19710067 <abdillahabdillah112@gmail.com>
2021-06-01 18:02:50 +0800

    Output Dengan sout

---
+++ Modul01/DataTypes2.java
@@ -0,0 +1,19 @@
+public class DataTypes2{
+ public static void main(String[]args){
+  String namaLengkap = "Abdillah";
+  String namaPanggilan = "Dillah";
+  int usia = 18;
+  int targetTahunKuliah = 4;
+  double ipk = 3.89764512;
+  char nilaiAbjad = 'A' ;
+  boolean tampan = true;
+
+  System.out.println("Nama lengkap : " + namaLengkap);
+System.out.println("Nama Panggilan : " + namaPanggilan);	
+System.out.println("Nama Usia : " + usia);
+System.out.println("Target Kuliah : " + targetTahunKuliah + " tahun");
+System.out.println("IPK : " + ipk);
+System.out.println("Nilai PBO : " + nilaiAbjad);
+System.out.println("Tampan : " + tampan);
+ }
+}
