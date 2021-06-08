6df982edbfafda15f14687a0d0859d68fce31179
abdillah19710067 <abdillahabdillah112@gmail.com>
2021-06-08 18:58:32 +0800

    Operasi Tipe String

--- Modul01/src/Main.java
+++ Modul01/src/Main.java
@@ -1,5 +1,7 @@
+import java.util.Locale;
+
 public class Main {
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         String namaDepan = "Ab";
         String namaTengah = "dil";
         String namaBelakang = "lah";
@@ -9,11 +11,27 @@ public class Main {
         char nilaiAbjad = 'A';
         boolean tampan = true;
 
-        char[] uniskaChar = {'U','N','I','S','K','A'};
+        char[] uniskaChar = {'U', 'N', 'I', 'S', 'K', 'A'};
         String uniskaString = new String(uniskaChar);
 
-        System.out.println(uniskaString);
+        String namaLengkap = namaDepan + " " + namaTengah + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5,10));
+        System.out.println(namaLengkap.replace("dil", "Bar"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
 
-        System.out.println(namaDepan + namaTengah + namaBelakang);
+        String[] namaArray = namaLengkap.split("");
+
+        for (String nama : namaArray) {
+            System.out.println(nama);
+
+            System.out.println(uniskaString);
+
+            System.out.println(namaDepan + namaTengah + namaBelakang);
+        }
     }
 }
