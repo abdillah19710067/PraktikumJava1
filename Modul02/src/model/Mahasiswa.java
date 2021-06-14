c30c363c4def127c060400e02311f999a1920c7f
abdillah19710067 <abdillahabdillah112@gmail.com>
2021-06-14 17:33:06 +0800

    Membuat Constructor

--- Modul02/src/model/Mahasiswa.java
+++ Modul02/src/model/Mahasiswa.java
@@ -8,36 +8,73 @@ public class Mahasiswa {
     String npm;
     String nama;
     Date tanggalLahir;
-    void tampilkanAtribut() {
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void Npm(String npm) {
+        this.npm = npm;
+    }
+
+    public void Nama(String nama) {
+        this.nama = nama;
+    }
+
+    public Date Tanggallahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggallahir(Date tanggallahir) {
+        this.tanggalLahir = tanggallahir;
+    }
+    void tampilkanAtribut(){
         String polaTanggal = "dd-MM-yyyy";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
 
-        System.out.println("Nama \t\t\t : " + this.nama);
-        System.out.println("Npm \t\t\t : " + this.npm);
-        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(this.tanggalLahir));
-
+        System.out.println("Nama \t\t\t : "+ this.nama);
+        System.out.println("NPM \t\t\t : "+ this.npm);
+        System.out.println("Tanggal Lahir : " + simpleDateFormat);
     }
 
-    void menyapa() {
-        System.out.println("Hai Nama Aku : " + this.nama);
-
+    String getNama(){
+        return nama;
     }
 
     int hitungUsia() {
         Calendar tanggalLahir = Calendar.getInstance();
         tanggalLahir.setTime(this.tanggalLahir);
-        Calendar hariIni = Calendar.getInstance();
+        Calendar tanggalIni = Calendar.getInstance();
 
-        int selisihTahun = hariIni.get(Calendar.YEAR) - tanggalLahir.get(Calendar.YEAR);
+        int tahunLahir = tanggalLahir.get(Calendar.YEAR);
+        int tahunIni = tanggalIni.get(Calendar.YEAR);
 
-        if (hariIni.get(Calendar.MONTH) < tanggalLahir.get(Calendar.MONTH)) {
+        int selisihTahun = tahunIni - tahunLahir;
+
+        int bulanLahir = tanggalLahir.get(Calendar.MONTH);
+        int bulanIni = tanggalIni.get(Calendar.MONTH);
+
+        if (bulanIni < bulanLahir) {
             selisihTahun--;
         } else {
-            if (hariIni.get(Calendar.MONTH) == tanggalLahir.get(Calendar.MONTH) && hariIni.get(Calendar.DAY_OF_MONTH)
-                    < tanggalLahir.get(Calendar.DAY_OF_MONTH)) {
-                selisihTahun--;
+            int tanggalTanggalLahir = tanggalLahir.get(Calendar.DAY_OF_MONTH);
+            int tanggalTanggaIni = tanggalIni.get(Calendar.DAY_OF_MONTH);
+            if (bulanIni == bulanLahir) {
+                if (tanggalTanggaIni < tanggalTanggalLahir) {
+                    selisihTahun--;
+                }
+
             }
         }
         return selisihTahun;
+    }
+
+    public Mahasiswa() {
+    }
+
+    public Mahasiswa(String npm, String nama, Date tanggalLahir) {
+        this.npm = npm;
+        this.nama = nama;
+        this.tanggalLahir = tanggalLahir;
     }
 }
--- Modul02/src/model/MahasiswaBeraksi.java
+++ Modul02/src/model/MahasiswaBeraksi.java
@@ -4,11 +4,12 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 public class MahasiswaBeraksi {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException{
 
         String polaTanggal = "dd-MM-yyyy";
-        SimpleDateFormat simpleDateFormat =new SimpleDateFormat(polaTanggal);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
 
         Mahasiswa mahasiswa = new Mahasiswa();
         mahasiswa.npm = "19710067";
@@ -18,6 +19,7 @@ public class MahasiswaBeraksi {
         System.out.println("Nama \t\t\t : " + mahasiswa.nama);
         System.out.println("Npm \t\t\t : " + mahasiswa.npm);
         System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa.tanggalLahir));
+        System.out.println("Usia : \t\t\t : " + mahasiswa.hitungUsia() + " Tahun");
         System.out.println("");
 
         Mahasiswa mahasiswa2 = new Mahasiswa();
@@ -28,6 +30,7 @@ public class MahasiswaBeraksi {
         System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
         System.out.println("Npm \t\t\t : " + mahasiswa2.npm);
         System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa2.tanggalLahir));
+        System.out.println("Usia : \t\t\t : " + mahasiswa2.hitungUsia() + " Tahun");
         System.out.println("");
 
         Mahasiswa mahasiswa3 = new Mahasiswa();
@@ -38,14 +41,7 @@ public class MahasiswaBeraksi {
         System.out.println("Nama \t\t\t : " + mahasiswa3.nama);
         System.out.println("Npm \t\t\t : " + mahasiswa3.npm);
         System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa3.tanggalLahir));
+        System.out.println("Usia : \t\t\t : " + mahasiswa3.hitungUsia() + " Tahun");
         System.out.println("");
-
-        mahasiswa.tampilkanAtribut();
-        mahasiswa.menyapa();
-        System.out.println("Usiaku : " + mahasiswa.hitungUsia() + " tahun");
-
-        Mahasiswa mahasiswa4 = new Mahasiswa();
-        System.out.println("");
-
     }
 }
