82b641be2dda48a39395d09473955ecba51d7b4d
abdillah19710067 <abdillahabdillah112@gmail.com>
2021-06-14 17:36:31 +0800

    Membuat Enkapsulasi

--- Modul02/src/model/Mahasiswa.java
+++ Modul02/src/model/Mahasiswa.java
@@ -9,6 +9,15 @@ public class Mahasiswa {
     String nama;
     Date tanggalLahir;
 
+    public Mahasiswa() {
+    }
+
+    public Mahasiswa(String npm, String nama, Date tanggalLahir) {
+        this.npm = npm;
+        this.nama = nama;
+        this.tanggalLahir = tanggalLahir;
+    }
+
     public String getNpm() {
         return npm;
     }
@@ -67,14 +76,5 @@ public class Mahasiswa {
             }
         }
         return selisihTahun;
-    }
-
-    public Mahasiswa() {
-    }
-
-    public Mahasiswa(String npm, String nama, Date tanggalLahir) {
-        this.npm = npm;
-        this.nama = nama;
-        this.tanggalLahir = tanggalLahir;
     }
 }
